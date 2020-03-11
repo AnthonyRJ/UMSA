212b0e5e8a32fe89b0fa22b988f9a76a1d7baa20
Anthony RJ <anthonyjoaquim40@gmail.com>
2020-03-11 23:26:30 +0100

    debut du front-end du reste de l'application

---
+++ app/src/main/java/com/example/Login_and_Registration/AccountCreationActivity.java
@@ -0,0 +1,14 @@
+package com.example.Login_and_Registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountCreationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_creation);
+    }
+}
