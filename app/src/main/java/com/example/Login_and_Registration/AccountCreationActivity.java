50f00413f92360656de77bf06cf3b3b868f5177c
Anthony RJ <anthonyjoaquim40@gmail.com>
2020-03-12 14:31:26 +0100

    ajout du systeme pour ajouter une appli dans la base de donnée

--- app/src/main/java/com/example/Login_and_Registration/AccountCreationActivity.java
+++ app/src/main/java/com/example/Login_and_Registration/AccountCreationActivity.java
@@ -2,13 +2,53 @@ package com.example.Login_and_Registration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AccountCreationActivity extends AppCompatActivity {
+
+    Button addApp;
+    EditText appName;
+
+    FirebaseFirestore db;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_creation);
+
+        addApp = (Button) findViewById(R.id.button_createAccount);
+        appName = (EditText) findViewById(R.id.editText_appName);
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        addApp.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                String nomApp = appName.getText().toString().trim();
+
+                Map<String, Object> data = new HashMap<>();
+                data.put("nomApplication", nomApp);
+
+                String path = nomApp + "_" + auth.getCurrentUser().getUid();
+                db.collection("Applications").document(path).set(data);
+
+                startActivity(new Intent(AccountCreationActivity.this, AccueilActivity.class));
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/Login_and_Registration/AccueilActivity.java
+++ app/src/main/java/com/example/Login_and_Registration/AccueilActivity.java
@@ -1,22 +1,33 @@
 package com.example.Login_and_Registration;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ScrollView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class AccueilActivity extends AppCompatActivity {
     Button logout;
-    Button test;
+    Button createApp;
+    Button reload;
+    ScrollView apps;
     FirebaseAuth firebaseAuth;
     FirebaseFirestore db;
     final Handler handler = new Handler();
@@ -26,7 +37,10 @@ public class AccueilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accueil_activity);
 
-        logout = findViewById(R.id.button_loggout);
+        logout = (Button) findViewById(R.id.button_loggout);
+        createApp = (Button) findViewById(R.id.button_createApp);
+        reload = (Button) findViewById(R.id.button_reload);
+        apps = (ScrollView) findViewById(R.id.scrollView_app);
         firebaseAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
 
@@ -44,6 +58,23 @@ public class AccueilActivity extends AppCompatActivity {
             }
         }, 1000);
 
+        reload.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                afficherApp();
+            }
+        });
+
+        createApp.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent moveToAccountCreation = new Intent(AccueilActivity.this, AccountCreationActivity.class);
+                startActivity(moveToAccountCreation);
+            }
+        });
+
         logout.setOnClickListener(new View.OnClickListener(){
 
             @Override
@@ -51,6 +82,23 @@ public class AccueilActivity extends AppCompatActivity {
 
                 FirebaseAuth.getInstance().signOut();
                 startActivity(new Intent(AccueilActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
+    public void afficherApp(){
+        List list = new ArrayList<>();
+        db.collection("Applications").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>(){
+
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (DocumentSnapshot ds : task.getResult()) {
+                        String id = ds.getId();
+                        //Test
+                        Log.d("TAG", id);
+                    }
+                }
             }
         });
     }
